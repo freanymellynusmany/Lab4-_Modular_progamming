d68eacd0318ab641053c36ba49682b7e4b710d78
freanymellynusmany <78341427+freanymellynusmany@users.noreply.github.com>
2021-03-02 12:02:06 +0700

    Add files via upload

---
+++ Modular_Progamming_Modul4.py
@@ -0,0 +1,22 @@
+#Freany mellyn usmany
+#71200644
+#univeristas kristen Duta Wacana
+
+#ada sebuah permainan taman anak-anak ada barisan bunga yang berbentuk segitiga
+#seorang anak menghitung alas dari segitiga tersebut yaitu 7 dan satu anak lagi mengukur tingginya  yaitu 9
+# kemudian anak satu nya lagi mengukur luas segitiga namun anak tersebut tidak bisa
+# buatlah program untuk mencari luas segitiga?
+
+
+#input
+#alassegitiga =input("Menghitung alas segitiga")
+#tinggisegitiga=input("menghitung tinggi segitiga")
+
+#proses 
+def luas_segitiga(alas,tinggi):
+    luas = alas*tinggi/2
+    return luas
+
+print("luas segitiga ;", luas_segitiga(7,9))
+
+
